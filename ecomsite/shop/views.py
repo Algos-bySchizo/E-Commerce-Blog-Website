6217559d535a5b298d2584ef8d15846f2fe48bd2
Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-06-16 18:11:51 +0500

    updates

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-    
+from django.http import HttpResponse
+
 def index(request):
     return render(request,'shop/index.html')
+def about(request):
+    # return render(request,'shop/index.html')
+    return HttpResponse('we\'re at about')
+def contact(request):
+    # return render(request,'shop/index.html')
+    return HttpResponse('we\'re at contact us')
+def tracker(request):
+    # return render(request,'shop/index.html')
+    return HttpResponse('we\'re at tracker')
+def search(request):
+    # return render(request,'shop/index.html')
+    return HttpResponse('we\'re at search')
+def prodview(request):
+    # return render(request,'shop/index.html')
+    return HttpResponse('we\'re at product view page')
+def checkout(request):
+    # return render(request,'shop/index.html')
+    return HttpResponse('we\'re at checkout page')
